fe3326c6c0890215cb25e224fb5d4ed44de60036
Swikriti Tripathi <swikriti808@gmail.com>
2022-01-05 14:00:52 +0545

    Comment added in the step

--- src/stepDefinitions/filesContext.js
+++ src/stepDefinitions/filesContext.js
@@ -100,7 +100,11 @@ Given('user {string} has renamed the following files', function(userId, table) {
 
 Given('user {string} has renamed file/folder {string} to {string}', webdav.move)
 
+// here regex is used so that it can capture steps that use escape characters on folder name
+// like : "folder with \"nested\" quote"
 Given(/user "([^"]*)" has created folder "(.+)"/gm, function(userId, folderName) {
+  // the captured regex won't filter the backslash present in steps with folder name "folder with \"nested\" quote"
+  // so we need to replace it and send it as `folder with "nested" quote`
   folderName = folderName.replaceAll("\\", "")
   return webdav.createFolder(userId, folderName)
 })
@@ -117,8 +121,11 @@ Given('user {string} has created file {string} on remote server', function(userI
   })
 })
 
-
+// here regex is used so that it can capture steps that use escape characters on file name
+// like : "file with \"nested\" quote"
 Given(/user "([^"]*)" has created file "(.+)"/, function(userId, fileName) {
+  // the captured regex won't filter the backslash present in steps with folder name "file with \"nested\" quote"
+  // so we need to replace it and send it as `file with "nested" quote`
   fileName = fileName.replaceAll("\\", "")
   return webdav.createFile(userId, fileName, '')
 })
