eee86938d2ddcafbc836ec3b6c9112972aa54574
Swikriti Tripathi <swikriti808@gmail.com>
2022-01-11 14:03:23 +0545

    Remove unnecessary steps from the middleware, minor fix to expiration date

--- src/stepDefinitions/webdavContext.js
+++ src/stepDefinitions/webdavContext.js
@@ -3,7 +3,6 @@ require('url-search-params-polyfill')
 const httpHelper = require('../helpers/httpHelper')
 const backendHelper = require('../helpers/backendHelper')
 const webdavHelper = require('../helpers/webdavHelper')
-const { client } = require('../config.js')
 const { xml2js } = require('xml-js')
 const _ = require('lodash')
 const assert = require('assert')
@@ -132,47 +131,6 @@ Then(/^as "([^"]*)" (file|folder) "([^"]*)" should exist inside folder "([^"]*)"
 ) {
   return fileOrFolderShouldExist(user, path.join(folder, file), resourceType)
 })
-
-Then('as {string} the last uploaded folder should exist', function(userId) {
-  return fileOrFolderShouldExist(userId, client.sessionId, 'folder')
-})
-
-Then(
-  'as {string} the last uploaded folder should contain the following files inside the sub-folders:',
-  async function(user, files) {
-    files = files.raw().map(item => item[0])
-
-    const sessionId = client.sessionId
-    const response = await webdavHelper.propfind(
-      `/files/${user}/${sessionId}`,
-      user,
-      [],
-      'infinity'
-    )
-    const result = xml2js(response, { compact: true })
-
-    const elements = _.get(result, 'd:multistatus.d:response')
-    if (elements === undefined) {
-      throw new Error('Received unexpected response:\n' + result)
-    }
-
-    const uploadedFilesHref = elements.map(elem => _.get(elem, 'd:href._text')).filter(item => item)
-
-    const regexToExtractBaseName = RegExp(`/${sessionId}/upload[0-9]+file/(.*)`)
-    const uploadedFilesWithBaseName = uploadedFilesHref
-      // unmatched items return null, else is array
-      .map(file => _.nth(file.match(regexToExtractBaseName), 1))
-      .filter(item => item)
-
-    const filesNotFoundInUploads = _.difference(files, uploadedFilesWithBaseName)
-    assert.strictEqual(
-      filesNotFoundInUploads.length,
-      0,
-      'Could not find following files inside sub-folders of the session folder\n' +
-        filesNotFoundInUploads
-    )
-  }
-)
 
 Given('user {string} has favorited element {string}', function(userId, element) {
   const body =
