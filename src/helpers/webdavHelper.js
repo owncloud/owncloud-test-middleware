de4e1ba968779224dab2558305077d333e151816
Swikriti Tripathi <swikriti808@gmail.com>
2022-01-12 12:45:13 +0545

    Fix getTrashBinElements function

--- src/helpers/webdavHelper.js
+++ src/helpers/webdavHelper.js
@@ -122,41 +122,54 @@ exports.propfind = function (path, userId, properties, folderDepth = '1') {
  * },...]
  *
  * @param {string} user
+ * @param depth
  */
-exports.getTrashBinElements = function (user, depth = 2) {
+exports.getTrashBinElements = function (user, depth = 'infinity') {
   return new Promise((resolve, reject) => {
     exports
-      .propfind(
-        `/trash-bin/${user}`,
-        user,
-        [
-          'oc:trashbin-original-filename',
-          'oc:trashbin-original-location',
-          'oc:trashbin-delete-timestamp',
-          'd:getlastmodified',
-        ],
-        depth
-      )
-      .then((str) => {
-        const trashData = convert.xml2js(str, { compact: true })['d:multistatus']['d:response']
-        const trashItems = []
-        trashData.map((trash) => {
-          if (trash['d:propstat']['d:prop'] === undefined) {
-            reject(new Error('trashbin data not defined'))
-          } else {
-            trashItems.push({
-              href: trash['d:href']._text,
-              originalFilename:
-                trash['d:propstat']['d:prop']['oc:trashbin-original-filename']._text,
-              originalLocation:
-                trash['d:propstat']['d:prop']['oc:trashbin-original-location']._text,
-              deleteTimestamp: trash['d:propstat']['d:prop']['oc:trashbin-delete-timestamp']._text,
-              lastModified: trash['d:propstat']['d:prop']['d:getlastmodified']._text,
+        .propfind(
+            `/trash-bin/${user}`,
+            user,
+            [
+              'oc:trashbin-original-filename',
+              'oc:trashbin-original-location',
+              'oc:trashbin-delete-timestamp',
+              'd:getlastmodified'
+            ],
+            depth
+        )
+        .then((str) => {
+          try {
+            let trashData = convert.xml2js(str, { compact: true })['d:multistatus']['d:response']
+            const trashItems = []
+            // 'trashData' gets object instead of array when there are no any files/folders in the trashbin
+            // so trashData.map() will cause error if trashData gets object
+            // following wraps the object in array
+            if (!Array.isArray(trashData)) {
+              trashData = [trashData]
+            }
+            trashData.forEach((trash) => {
+              if (trash['d:propstat']['d:prop'] === undefined) {
+                reject(new Error('trashbin data not defined'))
+              } else {
+                trashItems.push({
+                  href: trash['d:href']._text,
+                  originalFilename:
+                  trash['d:propstat']['d:prop']['oc:trashbin-original-filename']._text,
+                  originalLocation:
+                  trash['d:propstat']['d:prop']['oc:trashbin-original-location']._text,
+                  deleteTimestamp:
+                  trash['d:propstat']['d:prop']['oc:trashbin-delete-timestamp']._text,
+                  lastModified: trash['d:propstat']['d:prop']['d:getlastmodified']._text
+                })
+              }
             })
+            resolve(trashItems)
+          } catch (err) {
+            // Trying to read the nested properties can crash the test
+            reject(err)
           }
         })
-        resolve(trashItems)
-      })
   })
 }
 
